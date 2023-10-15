7898d538138cc771825d39c2c49f627b05bcf526
Kunal <kunalsabadra@gmail.com>
2023-10-15 12:28:35 +0530

    chage lift movement

--- src/js/index2.js
+++ src/js/index2.js
@@ -272,9 +272,10 @@ async function handleLift(door, next_floor) {
 
 
     Currentlift.isMoving = true;
+    console.log(Currentlift);
     let lift = document.querySelector(`#floorid-${door}`)
     // console.log({ lift })
-    lift.style.transform = `translateY(${temp}%)`;
+    lift.style.transform = `translateY(-${next_floor * 100}%)`;
     // lift.style.bottom = `${temp}%`
     lift.style.transition = `transform ${time}s`
 
